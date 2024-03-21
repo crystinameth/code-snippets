14a54c858f7ffc44ce76bd6a644d84e98c0b9ba0
shivimishra <shivimishra130103@gmail.com>
2024-03-21 18:01:11 +0530

    minor changes

--- frontend/code-snippets/app/page.tsx
+++ frontend/code-snippets/app/page.tsx
@@ -1,18 +1,21 @@
 import SnippetForm from "@/components/Form";
 import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-20">
-      <div className="items-right">
-        <Link href="/snippets">
-          <Button>View All Snippets</Button>
-        </Link>
-      </div>
-
+    <div className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold my-4">Code Snippets App</h1>
+        <div className="flex flex-row justify-between">
+          <h1 className="text-2xl font-bold my-4">Code Snippets App</h1>
+          <Button asChild variant="secondary">
+            <Link href="/snippets">
+              <Plus className="mr-2 h-4 w-4" />
+              View All Snippets
+            </Link>
+          </Button>
+        </div>
         <SnippetForm />
       </div>
     </div>
--- frontend/code-snippets/app/snippets/page.tsx
+++ frontend/code-snippets/app/snippets/page.tsx
@@ -1,25 +1,37 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import SnippetList, { Snippet } from "@/components/SnippetList";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Plus } from "lucide-react";
 
 export default function Snippets() {
   const [snippets, setSnippets] = useState<Snippet[]>([]);
   useEffect(() => {
     const fetchData = async () => {
-        try {
-          const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/snippets`);
-          const data = await response.json();
-          setSnippets(data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData();
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/snippets`
+        );
+        const data = await response.json();
+        setSnippets(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-bold my-4 text-primary/80">All Snippets</h1>
+    <div className="container mx-auto pt-10">
+      <div className="flex flex-row justify-between">
+        <h1 className="text-2xl font-bold my-4 text-primary/80">
+          All Snippets
+        </h1>
+        <Button asChild variant="secondary">
+          <Link href="/"><Plus className="mr-2 h-4 w-4" />Add New Snippet</Link>
+        </Button>
+      </div>
       <SnippetList snippets={snippets} />
     </div>
   );
